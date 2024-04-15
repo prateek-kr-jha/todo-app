1aed6b5d784e034f8a81a413da9ddbb3b7194b04
Prateek Kumar Jha <prateekj1211@gmail.com>
2024-04-15 23:47:58 +0530

    started building nav menu

--- src/script.js
+++ src/script.js
@@ -2,12 +2,20 @@ const nav = document.querySelector(".nav");
 console.log(nav);
 
 const navMenu = () => {
-    const projects = document.createElement('div');
-    projects.classList.add("projects");
-    const heading = document.createElement("h1");
-    heading.innerText = "My Projects";
-    projects.appendChild(heading);
-    return projects;
+    let outer_div = document.createElement("div");
+    for(let i = 0; i < 3; i++) {
+        let div = document.createElement("div");
+        div.classList.add("nav-category-container");
+        outer_div.appendChild(div);
+        if(i < 2) {
+            let line = document.createElement("div");
+            line.classList.add("line");
+            outer_div.appendChild(line)
+
+        }
+    }
+    
+    return outer_div;
 }
 nav.appendChild(navMenu());
 
