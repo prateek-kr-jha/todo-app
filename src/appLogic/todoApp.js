ce25e149a238ff3a3e9b75731d8469ee7fd611c5
Prateek <prateekj1211@gmail.com>
2024-07-02 00:07:18 +0530

    created addtask method in todoApp.js

--- src/appLogic/todoApp.js
+++ src/appLogic/todoApp.js
@@ -1,5 +1,18 @@
 import { Task, Project } from "./taskAndProject.js";
 
 class Todo {
-    
+    constructor() {
+        this.projects = {
+            inbox: new Project("inbox", "Add tasks to inbox on the go and organize later.")
+        };
+    }
+
+    addTask(newTask) {
+        if(!this.projects.hasOwnProperty(newTask.project)) {
+            throw new Error("No such project");
+        }
+        this.projects[newTask.project].tasks.push(newTask);
+    }
 }
+
+let todo_1 = new Todo();
