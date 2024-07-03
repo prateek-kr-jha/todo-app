508d8a0e609ba9590abcb08689aa81b310375094
Prateek <prateekj1211@gmail.com>
2024-07-04 00:39:52 +0530

    material ui test

--- src/script.js
+++ src/script.js
@@ -1,22 +1,3 @@
-const nav = document.querySelector(".nav");
-console.log(nav);
-
-const navMenu = () => {
-    let outer_div = document.createElement("div");
-    for(let i = 0; i < 3; i++) {
-        let div = document.createElement("div");
-        div.classList.add("nav-category-container");
-        outer_div.appendChild(div);
-        if(i < 2) {
-            let line = document.createElement("div");
-            line.classList.add("line");
-            outer_div.appendChild(line)
-
-        }
-    }
-    
-    return outer_div;
-}
-nav.appendChild(navMenu());
-
-
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
